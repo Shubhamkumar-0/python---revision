5442784602e2061a898963a7b08a4203475acc1a
Shubham Kumar <shubhamkumarerror1@gmail.com>
2025-08-11 04:56:36 +0000

    work with number

---
+++ Python/Unit-1/number.py
@@ -0,0 +1,28 @@
+import math
+
+sum_result = 5 + 3             # 8
+difference = 5 - 3              # 2
+product = 5 * 3                  # 15
+division_result = 5 / 2           # 2.5
+floor_division_result = 5 // 2     # 2
+modulus_result = 5 % 2              # 1
+exponentiation_result = 2 ** 3       # 8
+
+# absolute value.........
+abs_value = abs(-5)               # 5
+rounded_value = round(3.14159, 2)  # 3.14
+
+
+floor_value = math.floor(3.7)  # 3
+ceiling_value = math.ceil(3.7)  # 4
+
+# type conversion.......
+int_value = int(3.14)  # 3
+float_value = float(5)  # 5.0
+string_to_int = int("42")  # 42
+string_to_float = float("3.14")  # 3.14
+
+
+binary_value = bin(10)  # '0b1010'
+octal_value = oct(10)   # '0o12'
+hexadecimal_value = hex(10)  # '0xa'
