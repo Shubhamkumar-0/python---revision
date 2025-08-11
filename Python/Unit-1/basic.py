5f9da0f2c4762e8bf7b010d800844c99b75bdbc1
Shubham Kumar <shubhamkumarerror1@gmail.com>
2025-08-11 04:39:49 +0000

    adding some coding part and theory also

--- Python/Unit-1/basic.py
+++ Python/Unit-1/basic.py
@@ -11,3 +11,25 @@ for c in "chai":
 
 # mutabel-->we can change after they are created.(List, Dictionary, Set)
 # immutable-->we cannt change after they are created.(Tuple,String,Frozenset)
+
+# DATA TYPES........................
+# Number,float,complex,boolean,,string
+# list,tuple,set,disctionary,non type.
+
+
+
+# PYTHON VARIABLES...................
+# A variable is a symbolic name that stores a reference(pointer) to a value in memory.
+# It does not store the value directly in itself
+# Python variables are dynamically typed → you don’t declare the type explicitly, it’s inferred from the value.
+
+radius = 5
+pi = 3.14159
+area = pi * radius ** 2
+print(area)
+
+# Order of operator.....
+result = 2 + 3 * 4    # 14
+result2 = (2 + 3) * 4 # 20
+print(result)
+print(result2)
